119dfe704b69d6d634cdd12d838d3a58d227543e
casbear <69802192+casbear@users.noreply.github.com>
2021-10-27 11:49:18 +0200

    added sqlite database setup
    it makes a sqlite database and pushes all the link into it upon finishing scraping

--- sc/sc.py
+++ sc/sc.py
@@ -2,8 +2,21 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import sqlite3
+from sqlite3 import Error
+import urllib.parse
 
-import time
+#===============================================================================DATABASE SETUP
+
+connection = sqlite3.connect('C:/Users/kacpi/OneDrive/Dokumenty/lekcje/priv/sc/wikipedia.db')
+cur = connection.cursor()
+
+command1 = """CREATE TABLE IF NOT EXISTS
+    article(Title TEXT, Link TEXT)"""
+
+cur.execute(command1)
+
+#===============================================================================SCRAPER
 
 def getHTMLdocument(url):
 
@@ -12,30 +25,51 @@ def getHTMLdocument(url):
     return response.text
 
 toScrape = "https://pl.wikipedia.org/wiki/Portal:Historia"
-
 HTMLdocument = getHTMLdocument(toScrape)
-
 soup = BeautifulSoup(HTMLdocument, 'html.parser')
 
 connectQueue = []
-
+titleList = []
 i = 0
-while connectQueue != 0:
 
-
+while len(connectQueue) <= 3000:
 
     for link in soup.find_all('a', attrs={'href': re.compile("^/wiki/")}):
-
-        fullUrl = "https://pl.wikipedia.org" + link.get('href')
+    
+        url = link.get('href')
+        fullUrl = "https://pl.wikipedia.org" + url #getting full link to article
         connectQueue.append(fullUrl)
-        print(fullUrl)
-        connectQueue = list(set(connectQueue))
 
-
-    print("-----------------------------------------------------\n\n\n", connectQueue[i],"\n pages jumped:", i ,"\n articles collected:", len(connectQueue) ,"\n\n\n-----------------------------------------------------", end="")
 
     connectQueue = list(set(connectQueue))
-    soup = BeautifulSoup(getHTMLdocument(connectQueue[i]), 'html.parser')
+
+    soup = BeautifulSoup(getHTMLdocument(connectQueue[i]), 'html.parser')   
+
+    print("-----------------------------------------------------------------------------------------\n\n\n",
+         connectQueue[i],"\n pages jumped:", i ,"\n articles collected:", len(connectQueue) , 
+          "\n\n\n-----------------------------------------------------------------------------------------\n", end="")
+    
 
     i += 1
 
+#================================================================================
+
+for i in connectQueue:
+
+        #taking title from encoded link
+    encodedTitle = i
+    decodedTitle = urllib.parse.unquote(encodedTitle)
+    finishedTitle = decodedTitle.removeprefix("https://pl.wikipedia.org/wiki/")
+    finishedTitle = finishedTitle.replace("_", " ")
+    finishedTitle = finishedTitle.replace("'", " ")
+
+        #inserting to database
+    insert = "INSERT INTO article VALUES('" + str(finishedTitle) + "','" + str(i) + "')"
+    cur.execute(insert)
+
+    
+connection.commit()
+
+cur.execute('SELECT * FROM ARTICLE')
+result = cur.fetchall()
+print(result)
